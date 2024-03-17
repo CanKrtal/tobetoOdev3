956b7e83b8e34aad9afe803eefad11798714f4c9
Can Kartal <can.krtl05@gmail.com>
2024-03-18 00:01:34 +0300

    TobetoOdev3

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:deneme/homepage.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage()));
+}
